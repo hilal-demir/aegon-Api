31f9931e0224f7d973121360830d750d5104e815
bariszinkildak <baris.zinkildak@gmail.com>
2020-09-16 13:29:30 +0300

    txteditor path güncellendi

--- src/test/java/txtEditor.java
+++ src/test/java/txtEditor.java
@@ -1,27 +1,46 @@
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-
+import java.nio.file.Files;
 public class txtEditor {
 
     public String getPost(String scenarioName,String scenarioType){
-        String data="";
+//        String fileName = "config/sample.txt";
+
+//        String data="";
         String post="";
-        String filePath="home/batuhan.zafer/workspace/Documents/"+scenarioName+"/"+scenarioType+"/post.txt";
-        try {
-            File myObj = new File(filePath);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                 data = myReader.nextLine();
-                 post= post+data+"\n";
-              //  System.out.println("okunan deger="+data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            Assert.fail(filePath+" pathinde bir dosya bulunamadı "+e);
+        String filePath="Documents/"+scenarioName+"/"+scenarioType+"/post.txt";
+
+
+        String fileName = filePath;
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+
+            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            System.out.println(result);
+            post=result;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+//        try {
+//            File myObj = new File(filePath);
+//            Scanner myReader = new Scanner(myObj);
+//            while (myReader.hasNextLine()) {
+//                 data = myReader.nextLine();
+//                 post= post+data+"\n";
+//              //  System.out.println("okunan deger="+data);
+//            }
+//            myReader.close();
+//        } catch (FileNotFoundException e) {
+//            Assert.fail(filePath+" pathinde bir dosya bulunamadı "+e);
+//        }
 
         return post;
     }
@@ -29,21 +48,35 @@ public class txtEditor {
     public String getExpected(String scenarioName,String scenarioType){
         String data="";
         String post="";
-        String filePath="home/batuhan.zafer/workspace/Documents/"+scenarioName+"/"+scenarioType+"/expected.txt";
-        try {
-            File myObj = new File(filePath);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                data = myReader.nextLine();
-                post= post+data+"\n";
-                //  System.out.println("okunan deger="+data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            Assert.fail(filePath+" pathinde bir dosya bulunamadı"+ e);
+        String filePath="Documents/"+scenarioName+"/"+scenarioType+"/expected.txt";
+
+
+        String fileName = filePath;
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+
+            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            System.out.println(result);
+            post=result;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+//        try {
+//            File myObj = new File(filePath);
+//            Scanner myReader = new Scanner(myObj);
+//            while (myReader.hasNextLine()) {
+//                data = myReader.nextLine();
+//                post= post+data+"\n";
+//                //  System.out.println("okunan deger="+data);
+//            }
+//            myReader.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("An error occurred.");
+//            Assert.fail(filePath+" pathinde bir dosya bulunamadı"+ e);
+//        }
+//
         return post;
     }
 }
