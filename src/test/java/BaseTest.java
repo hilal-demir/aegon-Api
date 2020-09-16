69139e69cf7f2d58ed9e66ec9fc7a08df86b650b
bariszinkildak <baris.zinkildak@gmail.com>
2020-09-17 00:44:09 +0300

    base test system getenv

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -25,29 +25,37 @@ public class BaseTest {
         if (StringUtils.isEmpty(System.getenv("key"))) {
             logger.info("local koşum");
         } else {
+
             logger.info("Testinium üzerinden koşum");
             logger.info("Testinium üzerinden değişiklikler çekikiliyor.");
-            String changesNames= System.getenv("Changes");
-//
-//            for (int i=0; i<changesNames.length;i++){
-//                postChanges[i][1]=System.getenv(changesNames[i]);
-//                postChanges[i][0]=changesNames[i];
-//
-//            }
-//            int i=0;
-//            while (true){
-//                if(postChanges[i][0]==null){
-//                    break;
-//                }
-//                System.out.println("--------------------------------");
-//                System.out.println(postChanges[i][0]+"="+postChanges[i][1]);
-//                if(i>10){
-//                    System.out.println("i 10 u geçti bu durmuyor");
-//                    break;
-//                }
-//                i++;
-//            }
-//
+
+            String changes;
+            String changesNames[];
+            changes= System.getenv("Changes");
+            if(changes==null){
+                return;
+            }
+            changesNames=changes.split(",");
+
+            for (int i=0; i<changesNames.length;i++){
+                postChanges[i][1]=System.getenv(changesNames[i]);
+                postChanges[i][0]=changesNames[i];
+
+            }
+            int i=0;
+            while (true){
+                if(postChanges[i][0]==null){
+                    break;
+                }
+                System.out.println("--------------------------------");
+                System.out.println(postChanges[i][0]+"="+postChanges[i][1]);
+                if(i>10){
+                    System.out.println("i 10 u geçti bu durmuyor");
+                    break;
+                }
+                i++;
+            }
+
         }
         }
 
