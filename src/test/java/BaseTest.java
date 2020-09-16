d4dd06326247ad6ab398359f3e68c69e47bfa774
bariszinkildak <baris.zinkildak@gmail.com>
2020-09-17 00:55:26 +0300

    base test system getenv

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 public class BaseTest {
 
     public String[][] postChanges=new String[100][100];
+    public String[][] expectedChanges=new String[100][100];
+
     private Logger logger = LoggerFactory.getLogger(getClass());
     public static String scenarioName;
 
@@ -19,44 +21,71 @@ public class BaseTest {
     public void beforeScenario(ExecutionContext executionContext){
         scenarioName=executionContext.getCurrentScenario().getName();
         logger.info(
-            "\n+++++++++++++++++ Scenario Name +++++++++++++++++\n"+
+            "\n\n\n\n+++++++++++++++++ Scenario Name +++++++++++++++++\n"+
             "\n--------------"+scenarioName+"-----------------\n"+
-            "\n+++++++++++++++++++++++++++++++++++++++++++++++++\n");
+            "\n+++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n\n");
         if (StringUtils.isEmpty(System.getenv("key"))) {
             logger.info("local koşum");
         } else {
+            //Testinium üzerinden yapılan değişşiklikler güncelleniyor
+            setChangesForPost();
+            setChangesForExpected();
 
-            logger.info("Testinium üzerinden koşum");
-            logger.info("Testinium üzerinden değişiklikler çekikiliyor.");
-
-            String changes;
-            String changesNames[];
-            changes= System.getenv("Changes");
-            if(changes==null){
-                return;
-            }
-            changesNames=changes.split(",");
-
-            for (int i=0; i<changesNames.length;i++){
-                postChanges[i][1]=System.getenv(changesNames[i]);
-                postChanges[i][0]=changesNames[i];
-
-            }
-            int i=0;
-            while (true){
-                if(postChanges[i][0]==null){
-                    break;
-                }
-                System.out.println("--------------------------------");
-                System.out.println(postChanges[i][0]+"="+postChanges[i][1]);
-                if(i>10){
-                    System.out.println("i 10 u geçti bu durmuyor");
-                    break;
-                }
-                i++;
-            }
 
         }
         }
+        public void setChangesForPost(){
+        String changes;
+        String changesNames[];
+        changes= System.getenv("PostChanges");
+        if(changes==null){
+            logger.info("PostChanges için değişiklik yapılacak bir degişken bulunamadı");
+            return;
+        }
+        logger.info("PostChanges için veriler alınıyor.");
+
+        changesNames=changes.split(",");
+
+        for (int i=0; i<changesNames.length;i++){
+            postChanges[i][1]=System.getenv(changesNames[i]);
+            postChanges[i][0]=changesNames[i];
+
+        }
+        int i=0;
+        logger.info("Testinium üzerinden post için çekilen degişiklikler;");
+        while (true){
+            if(postChanges[i][0]==null){
+                break;
+            }
+            System.out.println(postChanges[i][0]+"="+postChanges[i][1]);
+        }
+    }
+
+    public void setChangesForExpected(){
+        String changes;
+        String changesNames[];
+        changes= System.getenv("ExpectedChanges");
+        if(changes==null){
+            logger.info("ExpectedChanges için değişiklik yapılacak bir degişken bulunamadı");
+            return;
+        }
+        logger.info("ExpectedChanges için veriler alınıyor.");
+
+        changesNames=changes.split(",");
+
+        for (int i=0; i<changesNames.length;i++){
+            expectedChanges[i][1]=System.getenv(changesNames[i]);
+            expectedChanges[i][0]=changesNames[i];
+
+        }
+        int i=0;
+        logger.info("Testinium üzerinden expected için çekilen degişiklikler;");
+        while (true){
+            if(expectedChanges[i][0]==null){
+                break;
+            }
+            System.out.println(expectedChanges[i][0]+"="+expectedChanges[i][1]);
+        }
+    }
 
 }
